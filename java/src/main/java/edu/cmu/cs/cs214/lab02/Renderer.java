7b012c84558c03beb5309e4e2cfe9d7890012c8f
ConnorJKY <keyoujiang2023@gmail.com>
2024-09-05 17:30:39 -0400

    lab2

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -1,17 +1,48 @@
 package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.*;
+import org.w3c.dom.css.Rect;
 
-public class Renderer {
-    public Rectangle rectangle;
-    
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
+public class Renderer<Shape> {
+    //    private Rectangle rectangle;
+    private Shape shape;
+
+    Renderer(Shape shape){
+        this.shape = shape;
+    }
+    public Shape getShape() {
+        return shape;
     }
 
-    void draw() {
-        double area = rectangle.getArea();
+    public void setShape(Shape shape) {
+        this.shape = shape;
+    }
+//    public double hei, wid;
+    
+//    Renderer(double h, double l) {
+//        this.hei = h;
+//        this.wid = l;
+//    }
 
+//    Renderer(Rectangle rectangle){
+//        this.hei = rectangle.height;
+//        this.wid = rectangle.width;
+//    }
+//    public void setRectangle(Rectangle rectangle){
+//        this.rectangle = rectangle;
+//    }
+//
+//    public Rectangle getRectangle(){
+//        return this.rectangle;
+//    }
+    public void draw() {
+        double area = 0.0;
+        if(this.shape instanceof Rectangle) {
+            area = ((Rectangle) this.shape).getArea();
+        }
+        if(this.shape instanceof Circle) {
+            area = ((Circle) this.shape).getArea();
+        }
         // assume implementation
 
         System.out.println("Shape printed\n" + "Its area is " + area);
